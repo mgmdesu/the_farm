b0d5bf1bda766a14a6e5e14aa57e1189963748e3
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-03-16 13:24:16 +0000

    [add]controller/views追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,36 @@
 Rails.application.routes.draw do
   
   scope module: :public do
+    resources :ships, except: [:new, :show]
+    resources :items, only: [:index, :show]
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post "confirm"
+        get "complete"
+      end 
+    end
+    
+    resources :cart_items, only: [:index, :create, :update, :destroy] do
+      collection do
+        delete "destroy_all"
+      end 
+    end 
+    
+    resource :customers, only: [:show, :edit, :update] do
+      get "unsubscribe"
+      patch "withdraw"
+    end 
+  end 
+  
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :items, except: [:destroy]
+    resources :customers, except: [:new, :show, :destory]
+    resources :orders, only: [:indes, :show, :update]
+    resources :order_details, only: [:update]
+  end
+  
+  scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about', as: 'about'
   end
