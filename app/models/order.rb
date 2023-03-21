f2fb9169a17500b736efb850ed549d840cc60ef0
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-03-21 01:08:11 +0000

    [Add]enum/日本語歌

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   
   belongs_to :customer
   has_many :order_items, dependent: :destroy
+  
+  enum payment_method: { credit_card: 0, transfer: 1, cash_on_delivery: 2, paypay: 3 }
+  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered:4 }
+  
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,4 +3,6 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
+  enum making_status: { preparation: 0, shipping: 1, transit: 2, arrived: 3 }
+  
 end
