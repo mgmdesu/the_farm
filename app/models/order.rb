b5148cd40d6098f4805cf056193e12091288d807
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-03-21 01:41:35 +0000

    [Add]モデル追記

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,12 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
   
+  # 小計計算
+  def subtotal 
+    item.tax_included_price * amount
+  end 
+  
+  # enum定義
   enum payment_method: { credit_card: 0, transfer: 1, cash_on_delivery: 2, paypay: 3 }
   enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered:4 }
   
