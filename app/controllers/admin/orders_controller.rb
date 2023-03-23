d38ec351a9da21f2003bc1afcd8ca5e22d54abcb
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2023-03-23 12:35:35 +0000

    [add]admin/order追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,21 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authentication_admin!
+  
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @total = 0
+    @order_items.each do |order_item|
+      @total += order_item.tax_included_price * order_item.amount
+    end 
+    @customer = @order.customer
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    if @order.status == "payment_confirmation"
+      @order.order_details.update(confting_status: "production_pending")
+    end 
+
 end
